f610015ec604270681c2b265a2adb0efd0457611
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-02-17 22:44:14 +0700

    feat: update finale yak e :V

--- script.js
+++ script.js
@@ -31,34 +31,16 @@ const cardCompItem = [
   },
 ];
 
-// var cardData = cardCompItem.map(x => x)
-// console.log(cardData)
-
-// for (let i = 0; i < cardCompItem.length; i++) {
-//   console.log(cardCompItem[i].icon)
-// }
-
-var outputHtml = "";
-// for (let i = 0; i < cardCompItem.length; i++) {
-//   // console.log(cardCompItem[i].icon)
-//   outputHtml += cardCompItem[i].icon
-// }
+var outputHtml = ""
 
 // loop array here
 for (let i = 0; i < cardCompItem.length; i++) {
-  outputHtml += '<div class="card" >';
-  outputHtml += '<div class="card-inside-feat" >';
-  outputHtml += '<i class="material-icons i-style">' + cardCompItem[i].icon + '</i>';
-  outputHtml += '<h1 class="no-padding-margin font-roboto">' + cardCompItem[i].title + '</h1>';
-  outputHtml += '</div>';
-  outputHtml += '</div>';
+  outputHtml += '<div class="card" >'
+  outputHtml += '<div class="card-inside-feat" >'
+  outputHtml += '<i class="material-icons i-style">' + cardCompItem[i].icon + '</i>'
+  outputHtml += '<h1 class="no-padding-margin font-roboto">' + cardCompItem[i].title + '</h1>'
+  outputHtml += '</div>'
+  outputHtml += '</div>'
 }
 
 document.getElementById('boyy').innerHTML = outputHtml
-
-// console.log(outputHtml)
-
-// var res = cardCompItem.filter(x => { return x.title === 'easy publish page' })
-// console.log(res)
-
-// var cards = document.getElementById('card-id')
